ce365b094ce66e92efc858ffdbdf0b5c07cddc63
King-Man Chow <kingmanchowswe@gmail.com>
2025-02-01 14:41:05 -0600

    file updates

--- swift_counter/ContentView.swift
+++ swift_counter/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var count = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Counter: \(count)")
+                .font(.largeTitle)
+                .padding()
+            
+            HStack {
+                Button(action: { count -= 1}) {
+                    Text("-")
+                        .font(.largeTitle)
+                        .frame(width: 50, height: 50)
+                        .background(Color.red.opacity(0.7))
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                }
+                Button(action: { count += 1}) {
+                    Text("+")
+                        .font(.largeTitle)
+                        .frame(width: 50, height: 50)
+                        .background(Color.red.opacity(0.7))
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
